1250aafe1a9ae7a703b02c4a6631317325d18be7
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-01-13 20:47:50 +0530

    Add Cart Compoment Items

---
+++ src/CartItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class CartItem extends React.Component {
+    constructor (){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+
+    render() {
+        const { price, title, qty} = this.state;
+        return(
+            <div className="cart-item">
+                <div className="left-block">
+                    <img style={styles.image} src="" alt=""/>                    
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize: 25}}>{title}</div>
+                    <div style={{ color: '#777'}}>Rs: {price}</div>
+                    <div style={{ color: '#777'}}>Qty: {qty}</div>
+                
+                <div className="cart-item-actions">
+                    {/* {Buttons} */}
+                    <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" alt="increase"/>
+                    <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" alt="decrease"/>
+                    <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png" alt="delete"/>
+                </div>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+const styles = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        backgroundColor: '#777'
+    }
+}
+
+export default CartItem;
