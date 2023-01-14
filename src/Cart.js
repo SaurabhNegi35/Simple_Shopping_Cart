eae84bfae5a1d146f3631d63df4e9856fa8699fb
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-01-14 19:20:57 +0530

    Added Added Props to Cart Items

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,56 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
+    constructor (){
+        super();
+        this.state = {
+        products: [
+        {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: '',
+            id: 1
+        },
+        {
+            price: 99,
+            title: 'Watch',
+            qty: 1,
+            img: '',
+            id: 2
+        },
+        {
+            price: 99999,
+            title: 'Laptop',
+            qty: 1,
+            img: '',
+            id: 3
+        },
+        {
+            price: 99999,
+            title: 'Charger',
+            qty: 1,
+            img: '',
+            id: 4
+        }
+    ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        // this.testing();
+    }
     render() {
+        const {products}= this.state
         return (
         <div className="cart">
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
+            {products.map((product) => {
+                return (
+                    <CartItem 
+                      product={product} 
+                      key={product.id}
+                    />
+                )
+            })};
+            
         </div>
         );
     }
